193cdca3b419e0f9fa053e58f4f7327bdb41e18d
gordon <wgordonw1@gmail.com>
2012-10-04 11:24:27 -0400

    rename "test_project" to "project"

--- project/project/settings.py
+++ project/project/settings.py
@@ -97,10 +97,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'test_project.urls'
+ROOT_URLCONF = 'project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'test_project.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 TEMPLATE_DIRS = (
     join(dirname(__file__), 'templates').replace('\\','/'),
