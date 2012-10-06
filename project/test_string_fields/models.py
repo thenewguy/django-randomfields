0939843d2b8ba50d1cad83e25a6250d7610f55a6
gordon <wgordonw1@gmail.com>
2012-10-06 14:50:57 -0400

    add basic tests.  use the charfield for testing because it can easily be created with an arbitrary number of possibilities

--- project/test_string_fields/models.py
+++ project/test_string_fields/models.py
@@ -8,3 +8,12 @@ class RandomCharFieldDemo(models.Model):
     
     def __unicode__(self):
         return u"%s" % self.id
+
+class TestPrimaryKey(models.Model):
+    id = RandomCharField(primary_key=True, max_length=10)
+
+class TestUnique(models.Model):
+    unique_field = RandomCharField(unique=True, max_length=10)
+
+class TestMinLengthPossibilities(models.Model):
+    data = RandomCharField(unique=True, max_length=2, min_length=1, valid_chars="ab")
--- project/test_string_fields/tests.py
+++ project/test_string_fields/tests.py
@@ -1,16 +1,59 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.db import IntegrityError
 from django.test import TestCase
+from models import TestPrimaryKey, TestUnique, TestMinLengthPossibilities
 
+class SaveTests(TestCase):
+    def test_auto_primary_key(self):
+        obj = TestPrimaryKey()
+        obj.save()
+        all_pks = TestPrimaryKey.objects.all().values_list("pk", flat=True)
+        self.assertIn(obj.pk, all_pks)
+    
+    def test_specified_primary_key(self):
+        pk = "test"
+        obj = TestPrimaryKey(pk=pk)
+        obj.save()
+        all_pks = TestPrimaryKey.objects.all().values_list("pk", flat=True)
+        self.assertIn(pk, all_pks)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_collision_correction(self):
+        obj1 = TestUnique()
+        obj1.save()
+        obj2 = TestUnique(unique_field=obj1.unique_field)
+        obj2.save()
+        all_values = TestUnique.objects.all().values_list("unique_field", flat=True)
+        self.assertIn(obj1.unique_field, all_values)
+        self.assertIn(obj2.unique_field, all_values)
+        self.assertNotEqual(obj1.pk, obj2.pk)
+    
+    def test_min_length_possibilities(self):
+        # ensure possibilities are calculated properly for min_length
+        obj1 = TestMinLengthPossibilities()
+        self.assertEqual(obj1._meta.get_field("data").valid_chars, "ab")
+        self.assertEqual(obj1._meta.get_field("data").max_length, 2)
+        self.assertEqual(obj1._meta.get_field("data").min_length, 1)
         """
-        Tests that 1 + 1 always equals 2.
+            possibilities:
+                a
+                aa
+                ab
+                ba
+                bb
+                b
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(6, obj1._meta.get_field("data").possibilities())
+        
+        # ensure all possibilities are saved
+        while TestMinLengthPossibilities.objects.count() < 6:
+            TestMinLengthPossibilities().save()
+        
+        all_values = TestMinLengthPossibilities.objects.all().values_list("data", flat=True)
+        self.assertIn('a', all_values)
+        self.assertIn('aa', all_values)
+        self.assertIn('ab', all_values)
+        self.assertIn('ba', all_values)
+        self.assertIn('bb', all_values)
+        self.assertIn('b', all_values)
+        
+        # ensure an integrity error is thrown on subsequent saves
+        self.assertRaises(IntegrityError, TestMinLengthPossibilities().save)
