8ef61ccf1137031c905b3bf50a6b16534951e558
gordon <wgordonw1@gmail.com>
2012-10-06 13:16:37 -0400

    move the variable length to another field.  keep the id a constant length

--- project/test_string_fields/models.py
+++ project/test_string_fields/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from randomfields.fields.string import RandomCharField
 
 class RandomCharFieldDemo(models.Model):
-    id = RandomCharField(primary_key=True, max_length=10, min_length=3)
+    id = RandomCharField(primary_key=True, max_length=10)
     unique = RandomCharField(unique=True, max_length=25)
-    normal = RandomCharField(max_length=3)
+    normal = RandomCharField(max_length=40, min_length=1)
     
     def __unicode__(self):
         return u"%s" % self.id
