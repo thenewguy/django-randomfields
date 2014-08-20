832b1fe5cf58f46a525bc0f47cac6fa004b2071c
gordon <wgordonw1@gmail.com>
2014-08-20 18:05:14 -0400

    adjust tests for the new approach with IntegerIdentifierBase

--- project/test_integer_fields/tests.py
+++ project/test_integer_fields/tests.py
@@ -40,10 +40,37 @@ class FieldTests(TestCase):
             self.assertGreaterEqual(value, field.lower_bound)
             self.assertLessEqual(value, field.upper_bound)
     
-    def test_integer_identifier_conversions(self):
-        for cls in [RandomBigIntegerIdentifierField, RandomIntegerIdentifierField, RandomSmallIntegerIdentifierField]:
-            field = cls()
-            for value in xrange(-20, 21, 1):
-                for x in [value, field.random()]:
-                    res = field.to_python(x)
-                    self.assertEqual(field.get_prep_value(res), x)
+    def _test_integer_identifier_conversions(self, field_cls, value_map):
+        field = field_cls()
+        for db_value, display_value in value_map:
+            result = field.to_python(db_value)
+            self.assertEqual(db_value, result.db_value)
+            self.assertEqual(display_value, result.display_value)
+            
+            result = field.to_python(display_value)
+            self.assertEqual(db_value, result.db_value)
+            self.assertEqual(display_value, result.display_value)
+    
+    def test_bigintegerfield_identifier_conversions(self):
+        value_map = (
+            (-9223372036854775808, 9223372036854775808),
+            (0, 18446744073709551616),
+            (9223372036854775807, 27670116110564327423),
+        )
+        self._test_integer_identifier_conversions(RandomBigIntegerIdentifierField, value_map)
+    
+    def test_integerfield_identifier_conversions(self):
+        value_map = (
+            (-2147483648, 2147483648),
+            (0, 4294967296),
+            (2147483647, 6442450943),
+        )
+        self._test_integer_identifier_conversions(RandomIntegerIdentifierField, value_map)
+
+    def test_smallintegerfield_identifier_conversions(self):
+        value_map = (
+            (-32768, 32768),
+            (0, 65536),
+            (32767, 98303),
+        )
+        self._test_integer_identifier_conversions(RandomSmallIntegerIdentifierField, value_map)
