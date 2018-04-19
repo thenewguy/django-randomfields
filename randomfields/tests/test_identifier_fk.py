354dec1053b5ab7e9ce62add6a54b863ed4ae824
gordon pendleton <gordon@internal.gandssecurityincorporated.com>
2018-04-19 17:23:13 -0400

    add more failing tests that target the source of the issue at hand

--- randomfields/tests/test_identifier_fk.py
+++ randomfields/tests/test_identifier_fk.py
@@ -1,6 +1,10 @@
+import copy
+import pickle
+
 from django.test import TestCase
 from randomfields.checks import DJANGO_VERSION_17
 from randomfields.tests.models import TestIdentifierValue, TestIdentifierFKValue
+from randomfields.models.fields.integer import IntegerIdentifier
 from unittest import skipIf
 
 
@@ -15,5 +19,27 @@ class IdentifierFKTests(TestCase):
         # ensure we don't raise a type error when accessing the reverse relation
         # encountering the following exception when we do at the moment:
         #    TypeError: __new__() missing 3 required positional arguments: 'possibilities', 'lower_bound', and 'upper_bound'
+        # this issue began to occur in Django 2.0 and seems to be an issue
+        # with copy/deepcopy and the IntegerIdentifier class
         instance.data
     
+    def test_integer_identifier_copy(self):
+        value = IntegerIdentifier(1, 3, -1, 1)
+        value_copy = copy.copy(value)
+        self.assertEqual(value, value_copy)
+
+    def test_integer_identifier_deepcopy(self):
+        value = IntegerIdentifier(1, 3, -1, 1)
+        value_deepcopy = copy.deepcopy(value)
+        self.assertEqual(value, value_deepcopy)
+    
+    def test_integer_identifier_pickle_dumps(self):
+        value = IntegerIdentifier(1, 3, -1, 1)
+        pickle.dumps(value)
+    
+    def test_integer_identifier_pickle_loads(self):
+        value = IntegerIdentifier(1, 3, -1, 1)
+        value_pickled = pickle.dumps(value)
+        value_unpickled = pickle.loads(value_pickled)
+        
+        self.assertEqual(value, value_unpickled)
