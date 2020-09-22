2e98bc8c67ce23c9b5d7543b9da2f69c29a02ffc
Gordon Pendleton <wgordonw1@gmail.com>
2020-09-22 16:09:03 -0400

    updated box as url we were using was removed

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,13 +6,17 @@ set -o xtrace
 
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get update
+
+apt-get install -y software-properties-common
+
 add-apt-repository ppa:deadsnakes/ppa
 
 apt-get update
 
-apt-get install -y git python3.5 python3.6
+apt-get install -y git python3.5 python3.6 python3.7
 
 curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+python3.6 get-pip.py
 
 pip install tox
